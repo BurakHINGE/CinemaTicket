2731a67c473a57365a905388d46914e5b5658fc9
Mehmet Burak Menteşe <burakmentese16@gmail.com>
2026-02-27 23:05:29 +0300

    Update and rename CinemaTicket.java to tr_CinemaTicket.java
    File and class name languages have been identified as Turkish.

--- Codes/tr_CinemaTicket.java
+++ Codes/tr_CinemaTicket.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class CinemaTicket {
+public class tr_CinemaTicket {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
